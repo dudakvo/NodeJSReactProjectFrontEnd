2fbac8c307505ba6ab849f4f2584fb8e4ce79707
lenakernytska <yaapril222@gmail.com>
2021-06-27 21:00:42 +0300

    Header done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Container from './components/Container';
-
+import Header from './components/Header';
 
 function App() {
   return (
-    (
     <Container>
-          
+      <Header/>    
       </Container>
-  )
   );
 }
 
