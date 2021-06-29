5b792ee846dbc15cb3278e1c36d306980cb88360
wal1m <wal1m@yandex.ru>
2021-06-29 16:55:49 +0300

    TaskPage
    модальное окно создание задания

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Container from './components/Container';
-
+import TaskPage from './pages/TaskPage';
+import ModalTaskPages from './components/ModalTaskPages/ModalTaskPages';
 
 function App() {
+  // const isOpen = Boolean(1);
   return (
-    (
     <Container>
-          
-      </Container>
-  )
+      <ModalTaskPages>
+        <TaskPage />
+      </ModalTaskPages>
+    </Container>
   );
 }
 
